f00a74bddb20e628704376d02a9d04407838c8a7
averyawesome555 <32853959+averyawesome555@users.noreply.github.com>
2020-01-31 07:48:59 -0500

    Update AppDelegate.swift

--- GitExample/AppDelegate.swift
+++ GitExample/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // hey guys, this is Austin
     // boom bam bop
+    // this line was added from the github text editor
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
