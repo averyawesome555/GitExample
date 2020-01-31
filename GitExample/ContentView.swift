e4c2eef872baf04a7dbe2e901a14db0519317049
Jack Shapiro <averyawesome555@gmail.com>
2020-01-31 07:42:28 -0500

    added comment

---
+++ GitExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitExample
+//
+//  Created by Jack Shapiro (student LM) on 1/31/20.
+//  Copyright © 2020 Big Brain Corporation. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
